24f1c280dab5013041dff3c961ada2b65401c941
DaemonLuzferim <evocryp.1.2@gmail.com>
2026-01-13 11:11:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from models import db
 from auth import auth
-from posts import posts
+from api_posts import posts
 import os
 
 app = Flask(__name__)
